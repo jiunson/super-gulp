02246fa29834318ad585f1f0b9de2be4134f016e
Ryan <jiunson@naver.com>
2022-11-14 16:14:32 +0900

    Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,31 +2,63 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
+
+const sass = require('gulp-sass')(require('sass'));
 
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",    
         dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
     }
 }
 
 // Tasks
 const pug = () => 
-    gulp.src(routes.pug.src)
+    gulp
+    .src(routes.pug.src)
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
+
 const clean = () => del(["build"]);
+
 const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}));
+
+const img = () => 
+    gulp
+    .src(routes.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+    gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
-    gulp.watch(routes.pug.watch, pug)
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
 };
 
-// 준비.
-const prepare = gulp.series([clean]);
+//----------------------------------------------------------------------------------- 
+
+// 준비과정.
+const prepare = gulp.series([clean, img]);
 
 // 컴파일.
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 // 웹서버 실행하고 파일의 변동사항을 지켜본다. 
 const postDev = gulp.parallel([webserver, watch]);
